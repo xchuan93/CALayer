10562ec3f3e425a6562c03c0bfc05bc6d1575e0b
xchuan <xchuan_93@163.com>
2019-04-08 11:08:21 +0800

    CAShaperLayer

---
+++ XCCAShaperLayer/XCCAShaperLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCCAShaperLayer
+//
+//  Created by xchuan on 2019/4/8.
+//  Copyright © 2019年 xchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCCAShaperLayer/XCCAShaperLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCCAShaperLayer
+//
+//  Created by xchuan on 2019/4/8.
+//  Copyright © 2019年 xchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
