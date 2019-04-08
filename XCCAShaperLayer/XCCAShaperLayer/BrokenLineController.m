956c3ba706687f64c4defe82f27e25f84b0eedbe
xchuan <xchuan_93@163.com>
2019-04-08 16:40:46 +0800

    折线图

--- XCCAShaperLayer/XCCAShaperLayer/BrokenLineController.m
+++ XCCAShaperLayer/XCCAShaperLayer/BrokenLineController.m
@@ -8,15 +8,61 @@
 
 #import "BrokenLineController.h"
 
+
+#define Height self.view.bounds.size.height / 2.0f
+
 @interface BrokenLineController ()
+
+@property (nonatomic, strong) CAShapeLayer *lineLayer;
+@property (nonatomic, strong) CAShapeLayer *midLineLayer;
 
 @end
 
 @implementation BrokenLineController
 
+
+- (CAShapeLayer *)lineLayer {
+    if (!_lineLayer) {
+        _lineLayer = [CAShapeLayer layer];
+        _lineLayer.lineWidth = 1.0f;
+        _lineLayer.fillColor = nil;
+        _lineLayer.strokeColor = [UIColor orangeColor].CGColor;
+        [self.view.layer addSublayer:_lineLayer];
+    }
+    return _lineLayer;
+}
+
+- (CAShapeLayer *)midLineLayer {
+    if (!_midLineLayer) {
+        _midLineLayer = [CAShapeLayer layer];
+        _midLineLayer.lineWidth = 1.0f;
+        _midLineLayer.fillColor = nil;
+        _midLineLayer.strokeColor = [UIColor redColor].CGColor;
+        [self.view.layer addSublayer:_midLineLayer];
+    }
+    return _midLineLayer;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(20, Height)];
+    for (NSInteger i = 0; i < 40; i++) {
+        NSInteger rand = arc4random() % 100;
+        if (i % 2 == 0) {
+            [path addLineToPoint:CGPointMake(20 + 10 * i, Height + rand)];
+        }else{
+            [path addLineToPoint:CGPointMake(20 + 10 * i, Height - rand)];
+        }
+    }
+    self.lineLayer.path = path.CGPath;
+    
+    UIBezierPath *midPath = [UIBezierPath bezierPath];
+    [midPath moveToPoint:CGPointMake(20, Height )];
+    [midPath addLineToPoint:CGPointMake(self.view.bounds.size.width - 20, Height)];
+    self.midLineLayer.path = midPath.CGPath;
 }
 
 /*
